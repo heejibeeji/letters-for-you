16dec57c50a69c93834780fd975944590a3132c5
heejibeeji <anonymurphy62@gmail.com>
2026-02-16 04:19:39 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const letters = [
+  {
+    date: "June 10, 2026 — 11:42 PM",
+    text: "This is the first letter.\nI hope this place feels like a secret just for us."
+  }
+];
+
+const container = document.getElementById("letters");
+
+letters.reverse().forEach(letter => {
+  const div = document.createElement("div");
+  div.className = "letter";
+
+  div.innerHTML = `
+    <div class="date">${letter.date}</div>
+    <div class="text">${letter.text}</div>
+  `;
+
+  container.appendChild(div);
+});
