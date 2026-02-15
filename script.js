ac9a8eb98092bd1166eac29edeccb1248c03c599
heejibeeji <anonymurphy62@gmail.com>
2026-02-16 04:24:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const letters = [
   {
+  date: "June 16, 2026 — 1:12 AM",
+  text: "Write whatever you feel here.\nNew lines work naturally.\nShe’ll see this instantly."
+},
+
+  {
     date: "June 10, 2026 — 11:42 PM",
     text: "This is the first letter.\nI hope this place feels like a secret just for us."
   }
